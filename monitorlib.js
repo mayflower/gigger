6e3aee37a851e65684263f9451fe33a7f1c04824
Felix Kaser <f.kaser@gmx.net>
2011-08-23 14:35:34 +0200

    generalized the monitorlib
    moved out some code so that requests can be made
    from the website including monitorlib.js

--- monitorlib.js
+++ monitorlib.js
@@ -13,16 +13,18 @@
 	//Class Gigger
 	Gigger.Gigger = function(service) {
 		this.client = new Faye.Client(service);
-		
-		//register for a specific event and publish to dispatcher that we want to have it
-		var eventRequest = {path: "/fayetest/webapp.html", element: "title_id", event: "click"};
-		this.client.subscribe((eventRequest.path+"~"+eventRequest.element+"@"+eventRequest.event).replace(".", "$"), function(event) {
-			console.log("event callback", event);
-		});
-		this.client.publish('/dispatch', eventRequest);
 	};
 
 	Gigger.Gigger.prototype = {
+		/** Call this function to request updates on a specific event.
+		 *  @param eventRequest: object containing path, element and event
+		 *                       Example: {path: "/foo/bar.html", element: "some_id", event: "click" */
+		requestEvent: function(eventRequest) {
+			this.client.subscribe((eventRequest.path+"~"+eventRequest.element+"@"+eventRequest.event).replace(".", "$"), function(event) {
+				console.log("event callback", event);
+			});
+			this.client.publish('/dispatch', eventRequest);
+		},
 		stop: function() {
 			console.log("stop gigger");
 		}
