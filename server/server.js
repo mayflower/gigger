e4307c383bb21ce2e8ad05d0a8b680fb6ed9dd2f
Felix Kaser <f.kaser@gmx.net>
2011-09-26 14:48:09 +0200

    added server side logging
    output faye log stuff, using the 'info' log level.

--- server/server.js
+++ server/server.js
@@ -1,6 +1,17 @@
 var http = require("http"),
 	faye = require("faye");
 
-var bayeux = new faye.NodeAdapter({mount: "/faye", timeout: 45});
+// log all infos
+faye.Logging.logLevel = 'info';
+faye.logger = function(msg) {
+	console.log('logged: ' + msg);
+}
+
+// create new faye node
+var bayeux = new faye.NodeAdapter({
+		mount: "/faye",
+		timeout: 45
+	});
+
 bayeux.listen(8000);
 
