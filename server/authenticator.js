3e10400d682e32cf0f026e772623d684c9304cd0
Felix Kaser <f.kaser@gmx.net>
2011-09-29 14:39:56 +0200

    added comment to exlain how to write auth

--- server/authenticator.js
+++ server/authenticator.js
@@ -1,3 +1,13 @@
+/** * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Implement your authentication algorithm here. You can
+ * query a database, generate some nice hashes, forward the
+ * authentication to another server, check session IDs, ...
+ *
+ * Just make sure to call callback either with true (if granted)
+ * or with false (it not granted). When the authorisation is not
+ * granted, you can optionally pass a reason with the callback.
+ * Like: callback(false, "wrong password, stupid")
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 function authenticate(auth, callback) {
 	if (auth && auth.user === 'test' && auth.password === 'gigger')
 		callback(true);
